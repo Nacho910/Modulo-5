4866789543f8e7920942e2b3d26e79cfef8aa252
Nacho910 <95248392+Nacho910@users.noreply.github.com>
2022-04-04 21:16:39 -0400

    Add files via upload

---
+++ ABP1/abp1.sql
@@ -0,0 +1,18 @@
+SELECT * FROM telovendo.vendedor;
+INSERT INTO telovendo.vendedor (IdCodigo, RUN, Nombre, Apellido, Fecha_nacimiento, Seccion, Salario)
+VALUES 
+('01', 136584839, 'Juan', 'Perez', 1987-05-02, 'Electronica', 100000),
+('02', 136984839, 'Luis', 'Perez', 1986-05-02, 'Tecnologia', 100500),
+('03', 136554839, 'Vicente', 'Perez', 1985-05-02, 'Educacion', 200152),
+('04', 136454839, 'Cristian', 'Perez', 1984-05-02, 'Construccion', 175300),
+('05', 136454839, 'Franklin', 'Perez', 1983-05-02, 'Tecnologia', 100000),
+('06', 145694839, 'Valentina', 'Perez', 1982-05-02, 'Alimentacion', 75000),
+('07', 136584589, 'Camila', 'Perez', 1981-05-02, 'Educacion', 99999),
+('08', 158964889, 'John', 'Perez', 1997-05-02, 'Gobierno', 1054878),
+('09', 168877659, 'Pierre', 'Perez', 1996-05-02, 'Tecnologia', 57236),
+('010', 17568839, 'Juan', 'Perez', 1995-05-02, 'Salud', 105789);
+
+
+
+
+
